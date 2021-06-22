5685042163af0a43945189306c37a449f7ef2c29
Lucas Martins <lucasshc@gmail.com>
2021-06-22 18:06:08 -0300

    update de exercicios js curso em video

---
+++ ex-js-curso-em-video/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite permitido MULTADO`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ ex-js-curso-em-video/ex012.js
@@ -0,0 +1,8 @@
+var idade = 68
+if (idade < 16){
+    console.log("Não vota")
+}else if (idade < 18 || idade > 67){
+    console.log("O voto é opicional")
+    } else{
+        console.log("O voto é obrigatório")
+    }
