30fd723d2331e911bca2e879f7a32cc3be94d27c
Lucas Martins <lucasshc@gmail.com>
2021-06-26 18:48:34 -0300

    Update ex021.js

--- ex-js-curso-em-video/ex021.js
+++ ex-js-curso-em-video/ex021.js
@@ -2,6 +2,7 @@ let num = [5, 8 , 2, 9, 3]
 //num.push(1)
 //num.sort()
 //console.log(num.length)
+//num.indexOf(4)
 for (let pos = 0; pos < num.length; pos++){
     console.log(num[pos])
 }
@@ -11,3 +12,4 @@ console.log(num)
 for (let pos in num){
     console.log(num[pos])
 }
+
