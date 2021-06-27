62ede5defc7f3286bf41e7aa14cbf093f4d55e04
Lucas Martins <lucasshc@gmail.com>
2021-06-26 21:42:05 -0300

    Create ex022.js

---
+++ ex-js-curso-em-video/ex022.js
@@ -0,0 +1,31 @@
+function parimp(n){
+    if (n%2==0){
+        return 'par'
+    }else {
+        return 'impar'
+    }
+}
+
+let res = parimp(11)
+console.log(res)
+
+function soma(n1 = 0, n2= 0){
+    return n1 + n2
+}
+console.log(soma(2,5))
+
+let v = function(x){
+    return x*2
+}
+
+console.log(v(5))
+
+function fatorial(n) {
+    let fat = 1
+    for (let c = n; c > 1; c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
