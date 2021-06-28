6606f5e5f50e314c5ce64bcc4f5e05628c7fdc0e
Lucas Martins <lucasshc@gmail.com>
2021-06-27 22:58:39 -0300

    Update js.js

--- ex-js-curso-em-video/ex23/js.js
+++ ex-js-curso-em-video/ex23/js.js
@@ -21,8 +21,44 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert("Tudo OK")
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado `
+        lista.appendChild(item)
     } else {
         window.alert("Valor inválido")
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores){
+            soma += valores[pos]
+            if(valores [pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores [pos < menor]){
+                menor  = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é de ${media}</p>`
+
+
+
     }
 }
